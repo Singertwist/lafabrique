c8873f639eb3062d51df116ff4f3aba13635df34
Singertwist <dark_rere@hotmail.fr>
2018-01-08 22:25:15 +0100

    Création du model panier et ajout à l'interface admin

--- fabrique/panier/admin.py
+++ fabrique/panier/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import PanierItem
 # Register your models here.
+
+class PanierItemAdmin(admin.ModelAdmin):
+	list_display   = ('timestamp', 'updated')
+
+
+admin.site.register(PanierItem, PanierItemAdmin)
--- fabrique/panier/models.py
+++ fabrique/panier/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from catalogue.models import Article
 # Create your models here.
+
+class PanierItem(models.Model):
+	panier_id = models.CharField(max_length=50)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Date de création')
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Date de mise à jour')
+	quantite = models.IntegerField(default=1)
+	article = models.ForeignKey('catalogue.Article', unique=False)
+
+	class Meta:
+		ordering = ['timestamp', 'updated']
+
