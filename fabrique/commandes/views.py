04d2f2b2b13a760da964398d24199c4c6fc4ab73
singertwist <regis.terpend@yahoo.fr>
2018-07-23 22:36:21 +0200

    Design de la page de validation de la commande

--- fabrique/commandes/views.py
+++ fabrique/commandes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from panier.cart  import Cart, ComposedCart, FinalComposedCart
 
 # Create your views here.
 
@@ -6,4 +7,10 @@ def checkout_account(request):
 	return render(request, "commandes/checkout-account.html", {})
 
 def orders_create(request):
-	return render(request, "commandes/orders-create.html", {})
+	if bool(request.session.get('final_composed_cart')) or bool(request.session.get('cart')): #Condition nécessaire pour affciher si les deux paniers, final_composed_cart et cart sont vides.
+		final_composed_cart = FinalComposedCart(request)
+		cart = Cart(request)
+	else:
+		final_composed_cart = None
+		cart = None
+	return render(request, "commandes/orders-create.html", {'cart':cart, 'final_composed_cart':final_composed_cart})
